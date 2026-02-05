2fb525a867d5bf4b6578a71435f4352c636e1342
Erik Behrends <behrends@users.noreply.github.com>
2026-02-05 18:09:29 +0100

    Städte speichern mit conf

--- storage.js
+++ storage.js
@@ -1,8 +1,22 @@
 import { keyInYNStrict } from 'readline-sync';
+import Conf from 'conf';
+
+const store = new Conf({
+  projectName: 'weather-tif25b',
+  configName: 'data',
+  cwd: '.',
+});
 
 export function saveCity(name) {
   const save = keyInYNStrict(`Soll "${name}" gespeichert werden?`);
   if (save) {
+    const cities = store.get('cities', []);
+    if (cities.includes(name)) {
+      console.log('Ist bereits gespeichert.');
+      return;
+    }
+    cities.push(name);
+    store.set('cities', cities);
     console.log('Wird gespeichert!');
   }
 }
